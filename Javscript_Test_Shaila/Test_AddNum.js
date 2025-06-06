75321fb3007fed7d71a3cb1ca9c0771018b63847
Shailashree K <shailashree.k@ringcentral.com>
2025-06-06 16:18:55 +0530

    Initial commit

---
+++ Javscript_Test_Shaila/Test_AddNum.js
@@ -0,0 +1,14 @@
+// Adding 2 numbers
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+
+console.log("The sum is: " + sum);
+// Subttacts 2 numbers
+let num4 = 18;
+let num3 = 12;
+let dif = num4 - num3;
+
+console.log("The dif is: " + dif);
+
+
